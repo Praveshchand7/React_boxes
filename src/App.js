abde0fd1c3b5681cc795c1a01e12313d0f7d614b
Chand Pravesh <s2200115@BC-C02GR0BFQ6LR-MacBook-Air.local>
2022-05-10 10:53:12 +0300

    Boxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+import Box from "./Box";
 
-function App() {
+const persons = [
+  {
+    name: "Pravesh",
+    title: "CEO",
+    age: 29,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "finnish", id: 2 },
+      { name: "nepali", id: 3 },
+    ],
+  },
+  {
+    name: "Kalam",
+    title: "Developer",
+    age: 25,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "finnish", id: 2 },
+      { name: "nepali", id: 3 },
+    ],
+  },
+  {
+    name: "Benzama",
+    title: "Designer",
+    age: 35,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "nepali", id: 2 },
+    ],
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        {persons.map((person) => (
+          <Box
+            key={person.name}
+            name={person.name}
+            title={person.title}
+            age={person.age}
+            lang={person.languages.map((lang) => (
+              <li key={lang.id}>{lang.name}</li>
+            ))}
+          />
+        ))}
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small> CopyRights </small>
+    </footer>
+  );
+};
+
+export default Footer;
